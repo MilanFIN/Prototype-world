43571272ab77438ad9d69e5a97e51d6401c6d4fa
milanfin <milan.makipaa@gmail.com>
2021-10-18 21:13:54 +0300

    support for multiple blocks per item

--- Scripts/Inventory.gd
+++ Scripts/Inventory.gd
@@ -3,14 +3,22 @@ extends Spatial
 
 var item = null
 var block = null
+var blockList = []
+var currentIndex = 0
 var placeMode = false
 
 onready var blockPreview = $BlockPreview
 onready var itemHolder = $ItemHolder
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func _process(delta):
+	if (Input.is_action_just_pressed("ToggleNext")):
+		cycleBlock()
 
 #load a new item to the left hand
 func setItem(i, amount):
@@ -21,8 +29,21 @@ func setItem(i, amount):
 
 	itemHolder.add_child(item)
 
+func cycleBlock():
+	if (len(item.blocks) <= 1):
+		return
+	else:
+		currentIndex += 1
+		if (currentIndex >= len(item.blocks)):
+			currentIndex = 0
+		for i in blockPreview.get_children():
+			i.queue_free()
+		block = load("res://Assets/Blocks/"+ item.blocks[currentIndex]+".tscn").instance()
+		blockPreview.add_child(block)
+
 func loadBlock():
-	block = load("res://Assets/Blocks/"+ item.block+".tscn").instance()
+	currentIndex = 0
+	block = load("res://Assets/Blocks/"+ item.blocks[currentIndex]+".tscn").instance()
 	blockPreview.add_child(block)
 
 #check if item can be placed, and return the block if it is placed
@@ -48,6 +69,7 @@ func cyclePlace():
 		for i in blockPreview.get_children():
 			i.queue_free()
 		block = null
+		blockList = []
 
 	if (placeMode):
 		loadBlock()
--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-export var block = "Block"
+export var blocks = ["Block"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
