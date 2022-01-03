28b3fbfdefc313e990a11aa4dbbe7b81eb01516f
milanfin <milan.makipaa@gmail.com>
2022-01-03 21:12:27 +0200

    license notice menu

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -4,3 +4,8 @@ extends Control
 func _on_Begin_pressed() -> void:
 	get_tree().change_scene("res://Game.tscn")
 	pass # Replace with function body.
+
+
+func _on_License_notice_pressed() -> void:
+	get_tree().change_scene("res://Licenses.tscn")
+	pass # Replace with function body.
